8fd3105c3d2c589d38432886a88fc0e1b2c05ee8
jallenINFO4430 <10833958@my.uvu.edu>
2022-03-15 22:25:03 -0600

    Trivia House project

---
+++ app/src/main/java/com/justinallen/triviahouse/Look.kt
@@ -0,0 +1,12 @@
+package com.justinallen.triviahouse
+
+class Look (input: String?) {
+    var result:String? = null
+    var resultType: String? = null
+
+    init {
+        var look: List<String>? = input?.split("|")
+        resultType = look?.get(0)
+        result = look?.get(1)
+    }
+}
